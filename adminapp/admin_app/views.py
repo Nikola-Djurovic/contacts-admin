03c7b44b101225b51cdfacf4a017f02bdae3d522
Nikola-Djurovic <ndjurovic0416rn@raf.rs>
2021-09-15 08:35:20 +0200

    final changes

--- adminapp/admin_app/views.py
+++ adminapp/admin_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from .models import Contact, Token, Login
+from .filters import OrderFilter
+from django.views.generic import ListView
 # Create your views here.
 def homepage(request):
-    return render(request, 'homepage.html')
+    contacts = Contact.objects.all()
+    tokens = Token.objects.all()
+    logins = Login.objects.all()
+    myFilter = OrderFilter(request.GET, queryset = contacts)
+    return render(request, 'homepage.html',{'total_contacts':contacts.count(),
+                                            'total_tokens':tokens.count(),
+                                            'total_logins':logins.count(),
+                                            'users_to_contacts':contacts.count()*1.0/logins.count(),
+                                            'user_sesions_to_number_of_users_ratio':logins.count()*0.1/contacts.count(),
+                                            'my_filter':myFilter})
+
--- adminapp/adminapp/settings.py
+++ adminapp/adminapp/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'admin_app'
+    'admin_app',
+    'django_filters',
+    'bootstrapform'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
